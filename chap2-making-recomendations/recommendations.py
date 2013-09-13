479b3864350862e43109876bac0e4a497eaa67e6
rafaelportela <rafaelportela90@gmail.com>
2013-09-13 01:31:41 -0300

    Pearson coefficient.

--- chap2-making-recomendations/recommendations.py
+++ chap2-making-recomendations/recommendations.py
@@ -18,3 +18,35 @@ def sim_distance(prefs, person1, person2):
     sum_of_squares = sum( [pow(prefs[person1][item] - prefs[person2][item], 2) for item in si] )
     return 1/ (1+ sqrt(sum_of_squares) )
 
+# Returns the Pearson correlation coefficient por p1 and p2
+def sim_pearson(prefs, p1, p2):
+    # Get the list of mutually rated items
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]: si[item] = 1
+
+    # Find the number of elements
+    n = len(si)
+
+    # If they have no ratings in common, return 0
+    if n == 0: return 0
+
+    # Add up all the preferences
+    sum1 = sum( [prefs[p1][it] for it in si] )
+    sum2 = sum( [prefs[p2][it] for it in si] )
+
+    # Sum up the squares
+    sum1Sq = sum( [ pow(prefs[p1][it], 2) for it in si] )
+    sum2Sq = sum( [ pow(prefs[p2][it], 2) for it in si] )
+
+    # Sum up the products
+    pSum = sum( [prefs[p1][it] * prefs[p2][it] for it in si] )
+
+    # Calcule Pearson score
+    num = pSum - (sum1 * sum2 / n)
+    den = sqrt( (sum1Sq-pow(sum1,2)/n) * (sum2Sq-pow(sum2,2)/n) )
+    if den == 0: return o
+    r = num / den
+
+    return r
+
