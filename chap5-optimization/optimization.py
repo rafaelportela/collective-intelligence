cfd03f62d497e142a480bf28fd2b42bfea0eaf01
rafaelportela <rafaelportela90@gmail.com>
2013-11-12 00:11:19 -0200

    Schedule cost function

--- chap5-optimization/optimization.py
+++ chap5-optimization/optimization.py
@@ -39,3 +39,42 @@ def printschedule(r):
 s = [1,4,3,2,7,3,6,3,2,4,5,3]
 printschedule(s)
 
+def schedulecost(sol):
+    totalprice = 0
+    latestarrival = 0
+    earliestdep = 24 * 60
+
+    for d in range(len(sol)/2):
+
+        # Get the inbound and outbound flights
+        origin = people[d][1]
+        outbound = flights[(origin, destination)][int(sol[2*d])]
+        returnf = flights[(origin, destination)][int(sol[2*d+1])]
+
+        # Total price is the price of all outbound and return flights
+        totalprice += outbound[2]
+        totalprice += returnf[2]
+
+        # Track the latest arrival and earlist departure
+        if latestarrival < getminutes(outbound[1]):
+            latestarrival = getminutes(outbound[1])
+        if earliestdep > getminutes(returnf[0]):
+            earliestdep = getminutes(returnf[0])
+
+    # Every person must wait at the airport until the latest person arrives
+    # They also must arrive at the same time and wait for their flights
+    totalwait = 0
+
+    for d in range (len(sol)/2):
+        origin = people[d][1]
+        outbound = flights[(origin, destination)][int(sol[2*d])]
+        returnf = flights[(origin, destination)][int(sol[2*d+1])]
+        totalwait += latestarrival - getminutes(outbound[1])
+        totalwait += getminutes(returnf[0]) - earliestdep
+
+    # Does this solution require an extra day of car rental? That'll be $50!
+    if latestarrival < earliestdep: totalprice + 5
+
+    return totalprice + totalwait
+
+print schedulecost(s)
