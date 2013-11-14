247d9cf1185ba264f4353e553084400960da20c5
rafaelportela <rafaelportela90@gmail.com>
2013-11-13 23:24:51 -0200

    Random and Hill Climb optimization.

--- chap5-optimization/optimization.py
+++ chap5-optimization/optimization.py
@@ -78,3 +78,50 @@ def schedulecost(sol):
     return totalprice + totalwait
 
 print schedulecost(s)
+
+def randomoptimize(domain, costf):
+    best = 999999999
+    bestr = None
+    for i in range (10000):
+        # Create a random solution
+        r = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+
+        # Get the cost
+        cost = costf(r)
+
+        #compare it to the best one so far
+        if cost < best:
+            best = cost
+            bestr = r
+
+    return r
+
+def hillclimb(domain, costf):
+    # Create a random solution
+    sol = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
+
+    # Main loop
+    while 1:
+
+        # Create list of neighboring solutions
+        neighbors = []
+        for j in range(len(domain)):
+            # One away in each direction
+            if sol[j] > domain[j][0]:
+                neighbors.append(sol[0:j] + [sol[j]-1] + sol[j+1:])
+            if sol[j] < domain[j][1]:
+                neighbors.append(sol[0:j] + [sol[j]+1] + sol[j+1:])
+
+            # See what the best solution amongst the neighbors is
+            current = costf(sol)
+            best = current
+            for j in range(len(neighbors)):
+                cost = costf(neighbors[j])
+                if cost < best:
+                    best = cost
+                    sol = neighbors[j]
+                # If there's no improvement, then we've reached the top
+                if best == current:
+                    break
+
+            return sol
