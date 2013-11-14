8da55e2d30a992018475be16460d1657b5db80a6
rafaelportela <rafaelportela90@gmail.com>
2013-11-13 23:56:36 -0200

    Simulated Annealing optimization.

--- chap5-optimization/optimization.py
+++ chap5-optimization/optimization.py
@@ -125,3 +125,39 @@ def hillclimb(domain, costf):
                     break
 
             return sol
+
+def annealingoptimize(domain, costf, T=10000.0, cool=0.95, step=1):
+    # Initialize the values randomly
+    # FIX: float(random.randint .. could it be float?
+    vec = [int(random.randint(domain[i][0], domain[i][1])) for i in range(len(domain))]
+    print vec
+
+    while T > 0.1:
+        # Choose one of the indices
+        i = random.randint(0, len(domain)-1)
+
+        # Choose a direction to change it
+        dir = step * (-1) ** int(round(random.random()))
+
+        # Create a new list with one of the values changed
+        vecb = vec[:]
+        vecb[i] += dir
+        if vecb[i] < domain[i][0]: vecb[i] = domain[i][0]
+        elif vecb[i] > domain[i][1]: vecb[i] = domain[i][1]
+
+        # Calculate the current cost and the new cost
+        ea = costf(vec)
+        eb = costf(vecb)
+
+        # Is it better, or does it make the probability cutoff?
+        if eb < ea:
+            vec = vecb
+        else:
+            p = pow(math.e, -(eb-ea)/T)
+            if random.random() < p:
+                vec = vecb
+
+            # Decrease the temperature
+            T = T * cool
+
+        return vec
